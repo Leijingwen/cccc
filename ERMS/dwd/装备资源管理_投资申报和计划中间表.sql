0d7364cffab66513d9c938a2c205fecd026ea877
zhuchen0629 <1194345334@qq.com>
2024-04-29 11:07:31 +0800

    （装备资源管理_投资申报和计划中间表 表名修改）

--- ERMS/dwd/装备资源管理_投资申报和计划中间表.sql
+++ ERMS/dwd/装备资源管理_投资申报和计划中间表.sql
@@ -23,7 +23,7 @@ from stg.stg_cccc_erms_invm_plan_year_apply2plan_f_d
 where etl_date = '${etl_date}';
 
 
-create table dwd.dwd_erms_invm_plan_year_apply1plan_d
+create table dwd.dwd_erms_invm_plan_year_apply2plan_d
 (
     apid          string comment '申报id',
     plid          string comment '计划id',
@@ -38,7 +38,7 @@ create table dwd.dwd_erms_invm_plan_year_apply1plan_d
 --装备资源管理_投资申报和计划中间表
 with a as (select *
            from ods.ods_cccc_erms_invm_plan_year_apply2plan_f_d)
---insert overwrite table dwd.dwd_erms_invm_plan_year_apply1plan_d partition(etl_date = '${etl_date}')
+--insert overwrite table dwd.dwd_erms_invm_plan_year_apply2plan_d partition(etl_date = '${etl_date}')
 select a.apid                                                 as apid --申报id
      , a.plid                                                 as plid --计划id
      , from_unixtime(unix_timestamp(), 'yyyy-MM-dd HH:mm:ss') as etl_time
