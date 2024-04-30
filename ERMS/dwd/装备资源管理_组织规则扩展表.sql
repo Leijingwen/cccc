2a446761d57d16ee2948fb212fd3f9daeeda6c50
zhuchen0629 <1194345334@qq.com>
2024-04-30 11:20:48 +0800

    组织规则扩展 调整字段顺序

--- ERMS/dwd/装备资源管理_组织规则扩展表.sql
+++ ERMS/dwd/装备资源管理_组织规则扩展表.sql
@@ -3,17 +3,17 @@ create table dwd.dim_erms_orgext_d
 (
     oid              STRING comment '机构id',
     poid             STRING comment '父机构id',
-    gpoid            STRING comment '分组所属的实体父机构id',
-    goid             STRING comment '分组所属的实体机构id',
     coid             STRING comment '隶属单位id',
     orule            STRING comment '行政组织规则码',
     sno              decimal(38, 0) comment '节点序号',
     type             STRING comment '机构类型',
     typeext          STRING comment '组织属性',
+    goid             STRING comment '分组所属的实体机构id',
+    gpoid            STRING comment '分组所属的实体父机构id',
+    grule            STRING comment '分组所属的实体机构规则码',
     mrut             STRING comment '最近更新时间',
     oper             STRING comment '操作员',
     note             STRING comment '备注',
-    grule            STRING comment 'grule',
     oname            STRING comment '单位名称',
     second_unit_id   STRING comment '二级单位id',
     second_unit_name STRING comment '二级单位名称',
@@ -27,22 +27,20 @@ create table dwd.dim_erms_orgext_d
 
 
 
-
-
 insert overwrite table dwd.dim_erms_orgext_d
 select org_rule.oid                                                        as oid,              --机构id
        org_rule.poid                                                       as poid,             --父机构id
-       org_rule.gpoid                                                      as gpoid,            --分组所属的实体父机构id
-       org_rule.goid                                                       as goid,             --分组所属的实体机构id
        org_rule.coid                                                       as coid,             --隶属单位id
        org_rule.orule                                                      as orule,            --行政组织规则码
        org_rule.sno                                                        as sno,              --节点序号
        org_rule.type                                                       as type,             --机构类型
        org_rule.typeext                                                    as typeext,          --组织属性
+       org_rule.goid                                                       as goid,             --分组所属的实体机构id
+       org_rule.gpoid                                                      as gpoid,            --分组所属的实体父机构id
+       org_rule.grule                                                      as grule,            --分组所属的实体机构规则码
        org_rule.mrut                                                       as mrut,             --最近更新时间
        org_rule.oper                                                       as oper,             --操作员
        org_rule.note                                                       as note,             --备注
-       org_rule.grule                                                      as grule,            --grule
        org.name                                                            as oname,            --单位名称
        org_rule.secend_unit_id                                             as second_unit_id,   --二级单位id
        org2.name                                                           as second_unit_name, --二级单位名称
@@ -76,3 +74,4 @@ from (select oid,
 
 
 
+select * from dwd.dim_erms_orgext_d;
